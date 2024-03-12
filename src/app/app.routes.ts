60761e9521f318e235be52a2a597e472a65aadfc
A Venkatesh <avenkateshanand1998@gmail.com>
2024-03-12 21:28:34 +0530

    Here I have added products list Component Create a products Lists #4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { ProductsComponent } from './Pages/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        redirectTo:"/Home",
+        pathMatch:"full"
+    },
+    {
+        path:"Home",
+        component:HomeComponent
+    },
+    {
+        path:"Login",
+        component:LoginComponent
+    },
+    {
+        path:"products",
+        component:ProductsComponent
+    }
+];
